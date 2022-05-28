84198d18d2723837a838b672b5af9758423d2a7a
Stephen Moore <judo2000@comcast.net>
2022-05-27 21:44:22 -0600

    Display generic google map on the page

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-import { User } from './User';
-import { Company } from './Company';
-
-const user = new User();
-console.log(user);
-
-const company = new Company();
-console.log(company);
+// import { User } from './User';
+// import { Company } from './Company';
+new google.maps.Map(document.getElementById('map'), {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
